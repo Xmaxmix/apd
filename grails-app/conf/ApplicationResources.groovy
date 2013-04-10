b354de6d194af91b0e097d9da5781f9cc637cbd4
Emiliano Masi <emiliano.masi@fiz-karlsruhe.de>
2013-04-10 11:23:13 +0200

    Resources management

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,1 +1,14 @@
-modules = { apd { } }
+modules = { 
+    apd { 
+        defaultBundle 'apd'
+        dependsOn "cssscreen, javascript"
+    }
+    
+    cssscreen {
+        resource url:'/css/vendor/bootstrap.css', bundle: 'screen'
+    }
+    
+    javascript {
+        resource url:'/js/vendor/bootstrap.js'
+    }
+}
