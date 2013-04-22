310a79ff4883125a3913c098791bbafbb05d77ca
Christian Herlambang <christian.herlambang@fiz-karlsruhe.de>
2013-04-22 14:28:37 +0200

    APD-7: remove 'use strict' as it breaks the script.
    Using 'use script' in the function breaks the script, because it complains
    about Tooltip being undefined.

--- web-app/js/tooltip.js
+++ web-app/js/tooltip.js
@@ -15,65 +15,63 @@
  */
 //tooltip function
 $(function() {
-  'use strict';
-  Tooltip = function(element) {
-    this.init(element);
-  };
+    Tooltip = function(element) {
+        this.init(element);
+    };
 
-  $.extend(Tooltip.prototype, {
+    $.extend(Tooltip.prototype, {
 
-    hint: null,
-    tooltip: null,
+        hint: null,
+        tooltip: null,
 
-    opened: false,
-    lock: false,
+        opened: false,
+        lock: false,
 
-    hoverTime: 0,
-    hoverTimeout: 300,
+        hoverTime: 0,
+        hoverTimeout: 300,
 
-    init: function(element) {
-      var currObjInstance = this;
-      this.hint = element.attr('data-content');
-      this.tooltip = element.siblings('div.tooltip');
-      element.removeAttr('title');
-      this.tooltip.html(this.hint).text();
-      this.tooltip.hide();
-      this.tooltip.removeClass('off');
-      element.hover(function() {
-        var d = new Date();
-        currObjInstance.hoverTime = d.getTime();
-        currObjInstance.open();
-      });
-      this.tooltip.mouseenter(function() {
-        currObjInstance.lock = true;
-      });
-      this.tooltip.mouseleave(function() {
-        currObjInstance.close();
-      });
-      element.mouseleave(function() {
-        setTimeout(function() {
-          var currentD = new Date();
-          if (!currObjInstance.lock && (currObjInstance.hoverTime + currObjInstance.hoverTimeout - 100 < currentD.getTime())) {
-            currObjInstance.close();
-          }
-      },currObjInstance.hoverTimeout);
-      });
-    },
-    open: function() {
-      var currObjInstance = this;
-      if (!this.opened) {
-        this.opened = true;
-        this.tooltip.fadeIn('fast');
-      }
-    },
-    close: function() {
-      this.tooltip.fadeOut('fast');
-      this.opened = false;
-      this.lock = false;
-    }
-  });
+        init: function(element) {
+            var currObjInstance = this;
+            this.hint = element.attr('data-content');
+            this.tooltip = element.siblings('div.tooltip');
+            element.removeAttr('title');
+            this.tooltip.html(this.hint).text();
+            this.tooltip.hide();
+            this.tooltip.removeClass('off');
+            element.hover(function() {
+                var d = new Date();
+                currObjInstance.hoverTime = d.getTime();
+                currObjInstance.open();
+            });
+            this.tooltip.mouseenter(function() {
+                currObjInstance.lock = true;
+            });
+            this.tooltip.mouseleave(function() {
+                currObjInstance.close();
+            });
+            element.mouseleave(function() {
+                setTimeout(function() {
+                    var currentD = new Date();
+                    if (!currObjInstance.lock && currObjInstance.hoverTime + currObjInstance.hoverTimeout - 100 < currentD.getTime())
+                        currObjInstance.close();
+                },currObjInstance.hoverTimeout);
+            });
+        },
+        open: function() {
+            var currObjInstance = this;
+            if (!this.opened) {
+                this.opened = true;
+                this.tooltip.fadeIn('fast');
+            }
+        },
+        close: function() {
+            this.tooltip.fadeOut('fast');
+            this.opened = false;
+            this.lock = false;
+        }
+    });
 
-  $('span.contextual-help').each(function() {
-    new Tooltip($(this));
-  });
+    $('span.contextual-help').each(function() {
+        new Tooltip($(this));
+    });
 });
