2066def5f8fa2d4cc53be65cce8dbfd22a8f6277
Emiliano Masi <emiliano.masi@fiz-karlsruhe.de>
2013-04-29 13:22:16 +0200

    Review changes done

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -1,6 +1,5 @@
 //Initialization of search widget
 if ($('body').find('#home-search-container').length > 0) {
-    searchWidget = new SearchWidget($('#search-widget-form'),
-            $('#search-widget'), $('#search-widget')
-                    .find('.controls-container'));
+    var searchWidgetContainer = $('#search-widget-form');
+    searchWidget = new SearchWidget($('#search-widget-form'),searchWidgetContainer, searchWidgetContainer.find('.controls-container'));
 }
--- web-app/js/searchwidget.js
+++ web-app/js/searchwidget.js
@@ -1,63 +1,61 @@
 //Search widget
 SearchWidget = function(form, widget, controls) {
-    this.init(form, widget, controls);
+  this.init(form, widget, controls);
 }
 
 $.extend(SearchWidget.prototype, {
 
-    connectedForm : null,
-    connectedControlsContainer : null,
-    connectedWidgetContainer : null,
-    selectedValues : new Array(),
+  connectedForm : null,
+  connectedControlsContainer : null,
+  connectedWidgetContainer : null,
+  selectedValues : new Array(),
 
-    init : function(form, widget, controls) {
-        currObjInstance = this;
-        this.connectedForm = form;
-        this.connectedWidgetContainer = widget;
-        this.connectedControlsContainer = controls;
-        this.selectedValues.push('default');
+  init : function(form, widget, controls) {
+    var currObjInstance = this;
+    this.connectedForm = form;
+    this.connectedWidgetContainer = widget;
+    this.connectedControlsContainer = controls;
+    this.selectedValues.push('default');
 
-        this.connectedControlsContainer.find('select').each(
-                function() {
-                    $(this).change(
-                            function() {
-                                if ($(this).val() == "default") {
-                                    $(this).addClass("empty");
-                                } else {
-                                    $(this).removeClass("empty");
-                                }
-                                var filterValue = $(this).find('option:selected').attr('value');
-                                var content = this.value;
-                                if (jQuery.inArray(filterValue,
-                                        currObjInstance.selectedValues) == -1) {
-                                    currObjInstance.renderSelection(filterValue, content);
-                                    currObjInstance.selectedValues.push(filterValue);
-                                }
-                            });
-                });
+    this.connectedControlsContainer.find('select').each(function() {
+      $(this).change(function() {
+        if ($(this).val() == "default") {
+          $(this).addClass("empty");
+        }
+        else {
+          $(this).removeClass("empty");
+        }
+        var filterValue = $(this).find('option:selected').attr('value');
+        var content = this.value;
+        if (jQuery.inArray(filterValue, currObjInstance.selectedValues) == -1) {
+          currObjInstance.renderSelection(filterValue, content);
+          currObjInstance.selectedValues.push(filterValue);
+        }
+      });
+    });
 
-    },
-    renderSelection : function(filterValue, content) {
+  },
+  renderSelection : function(filterValue, content) {
 
-        currObjInstance = this;
+    var currObjInstance = this;
 
-        // Create hidden input
-        var hiddenInput = $(document.createElement('input'));
-        hiddenInput.attr('type', 'hidden');
-        hiddenInput.attr('name', 'filter')
-        hiddenInput.attr('value', filterValue);
-        this.connectedForm.append(hiddenInput)
+    // Create hidden input
+    var hiddenInput = $(document.createElement('input'));
+    hiddenInput.attr('type', 'hidden');
+    hiddenInput.attr('name', 'filter')
+    hiddenInput.attr('value', filterValue);
+    this.connectedForm.append(hiddenInput)
 
-        // Create pillbox
-        var li = $(document.createElement('li'));
-        li.attr('data-value', filterValue);
-        li.html(content);
-        this.connectedWidgetContainer.find('ul').append(li);
-        li.mouseup(function() {
-            currObjInstance.selectedValues = jQuery.grep(
-                    currObjInstance.selectedValues, function(value) {
-                        return value != filterValue;
-                    });
-        });
-    }
+    // Create pillbox
+    var li = $(document.createElement('li'));
+    li.attr('data-value', filterValue);
+    li.html(content);
+    this.connectedWidgetContainer.find('ul').append(li);
+    li.mouseup(function() {
+      currObjInstance.selectedValues = jQuery.grep(
+          currObjInstance.selectedValues, function(value) {
+            return value != filterValue;
+          });
+    });
+  }
 });
