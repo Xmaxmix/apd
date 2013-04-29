18f1dd8b006d2e9079fc05e1251b5a97a84ce1fb
Emiliano Masi <emiliano.masi@fiz-karlsruhe.de>
2013-04-29 14:02:07 +0200

    APD-55 branch correctly merged

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -1,5 +1,5 @@
 //Initialization of search widget
 if ($('body').find('#home-search-container').length > 0) {
-    var searchWidgetContainer = $('#search-widget-form');
+    var searchWidgetContainer = $('#search-widget');
     searchWidget = new SearchWidget($('#search-widget-form'),searchWidgetContainer, searchWidgetContainer.find('.controls-container'));
 }
